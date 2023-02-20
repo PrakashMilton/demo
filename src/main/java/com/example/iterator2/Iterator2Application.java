7563ab21aebd3a77fee3c1e32068e5994cadfe67
PrakashMilton <iamprakash4you@gmail.com>
2023-02-20 12:42:29 +0530

    first commit

---
+++ src/main/java/com/example/iterator2/Iterator2Application.java
@@ -0,0 +1,13 @@
+package com.example.iterator2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iterator2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iterator2Application.class, args);
+	}
+
+}
